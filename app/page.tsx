0abb905806a78134897415efeb5a25dfeb17b817
sincerelyyyash <yta0088@gmail.com>
2024-09-07 23:56:17 +0530

    Complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 import WorkflowBuilder from '@/components/WorkflowBuilder';
+import { Hero } from '@/components/Hero';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow p-4">
-        <WorkflowBuilder />
+        <Hero />
       </main>
     </div>
   );
